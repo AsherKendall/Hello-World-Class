2b90bea4e78784d6173584e44bbb07bf50ed37e5
Asher Griess <Asher.Griess@trojans.dsu.edu>
2024-01-28 14:03:27 -0600

    Added basic menu and finished step two.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,38 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                string menuSelect = Console.ReadLine();
+                Console.WriteLine("1: Select Random Number 1-10");
+                Console.WriteLine("2: ");
+                Console.WriteLine("3: ");
+                Console.WriteLine("4: ");
+                Console.WriteLine(); //New Line
+
+
+                if (menuSelect == "1")
+                {
+                    var rand = new Random();
+                    int num = rand.Next(1,11);
+                    Console.WriteLine(num);
+                } else if(menuSelect == "2")
+                {
+
+                }
+                else if (menuSelect == "3")
+                {
+
+                }
+                else if (menuSelect == "4")
+                {
+
+                }else
+                {
+                    Console.WriteLine("Try Again.");
+                }
+            }
+
         }
     }
 }
